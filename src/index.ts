cb072f71dd48fd898130dc2f08899924b2ff10db
b-bianca <you@example.com>
2021-02-22 14:21:09 -0300

    config iniciais banco e dados e .env

---
+++ src/index.ts
@@ -0,0 +1,24 @@
+/******************** IMPORTS ********************/ 
+import express, { Express } from "express"
+import cors from "cors"
+import { AddressInfo } from "net";
+
+
+/******************** CONFIG ********************/ 
+const app: Express = express()
+app.use(express.json())
+app.use(cors())
+
+
+/******************** ROUTES ********************/ 
+
+
+/******************** SERVER INIT ********************/ 
+const server = app.listen(3003, () => {
+    if (server) {
+       const address = server.address() as AddressInfo;
+       console.log(`Server running on http://localhost:${address.port}`);
+    } else {
+       console.error(`Failed to run the server.`)
+    }
+})
